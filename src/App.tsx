f9c415ab9e64fea3fc4e447aab71e2fb1a55a23b
alinaguseva1997 <alinagorbunova93@gmail.com>
2023-06-05 15:27:24 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,26 @@
-import React, {useState} from 'react';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import {Accordion} from "./components/Accordion/Accordion";
-import {Rating} from "./components/Rating/Rating";
-import {UnControlledOnOff} from "./components/UnControlledOnOff/UnControlledOnOff";
-import {OnOff} from "./components/OnOff/OnOff";
-import {UnControlledAccordion} from "./components/UnControlledAccordion/UnControlledAccordion";
-import {UnControlledRating} from "./components/UnControlledRating/UnControlledRating";
-
-
-
-export type RatingValueType = 0 | 1 | 2 | 3 | 4 | 5
 
 function App() {
-  console.log('App rendered')
-
-  let [ratingValue, setRatingValue] = useState<RatingValueType>(0)
-  let [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(false)
-  let [on, setOn] = useState<boolean>(false)
-
   return (
-      <div className={'App'}>
-        <UnControlledOnOff onChange = {setOn}/> {on.toString()}
-        <OnOff valueOnOff = {on} onClick = {setOn}/>
-        <UnControlledAccordion titleValue = {'Menu'}/>
-        <Rating value = {ratingValue} onClick = {setRatingValue}/>
-
-        <UnControlledRating />
-        <Accordion titleValue = {'Menu'} collapsed={accordionCollapsed} onClick = {() => setAccordionCollapsed(!accordionCollapsed)}/>
-
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-}
-
-type PageTitlePropsType = {
-  title: string
-}
-
-function PageTitle(props: PageTitlePropsType) {
-  console.log('PageTitle rendered')
-  return <h1>{props.title}</h1>
 }
 
 export default App;
