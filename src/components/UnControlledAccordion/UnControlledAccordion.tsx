5c0f0ce678942b8f0b646a4ec0edaf7932567d67
alinaguseva1997 <alinagorbunova93@gmail.com>
2023-06-05 15:38:52 +0300

    new components

---
+++ src/components/UnControlledAccordion/UnControlledAccordion.tsx
@@ -0,0 +1,43 @@
+import React, {useState} from "react";
+
+type AccordionPropsType = {
+    titleValue: string
+    //collapsed: boolean
+}
+
+export function UnControlledAccordion(props: AccordionPropsType) {
+    console.log('UnControlledAccordion rendered')
+
+    let [collapsed, setCollapsed] = useState(false)
+
+
+    return (
+        <div className={'App'}>
+            <AccordionTitle title={props.titleValue} onClick = {() => {setCollapsed(!collapsed)}}/>
+            {!collapsed && <AccordionBody/>}
+        </div>
+    )
+}
+
+type AccordionTitlePropsType = {
+    title: string
+    onClick: () => void
+}
+
+function AccordionTitle(props: AccordionTitlePropsType) {
+    console.log('AccordionTitle rendered')
+    return (
+        <h3 onClick={()=> {props.onClick()}}>{props.title}</h3>
+    )
+}
+
+function AccordionBody() {
+    console.log('AccordionBody rendered')
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
