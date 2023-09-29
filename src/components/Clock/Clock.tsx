fee853a96b0b2f993b635d85ea846d3fc58ae9cc
alinaguseva1997 <alinagorbunova93@gmail.com>
2023-09-29 18:19:21 +0300

    Clock Example Storybook

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -2,21 +2,27 @@ import React, {useEffect, useState} from "react";
 
 export type PropsType = {}
 
+const get2digitsString = (num: number) => num < 10 ? '0' + num : num
+
 export const Clock: React.FC<PropsType> = (props) => {
 
     const [date, setDate] = useState(new Date())
 
     useEffect(()=> {
-        setInterval(()=>{
+       const intervalID = setInterval(()=>{
             setDate(new Date())
         }, 1000)
-    })
+
+        return () => {
+            clearInterval(intervalID)
+        }
+    }, [])
 
     return <div>
-        <span>{date.getHours()}</span>
+        <span>{get2digitsString(date.getHours())}</span>
         :
-        <span>{date.getMinutes()}</span>
+        <span>{get2digitsString(date.getMinutes())}</span>
         :
-        <span>{date.getSeconds()}</span>
+        <span>{get2digitsString(date.getSeconds())}</span>
     </div>
 }
